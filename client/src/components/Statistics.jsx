8cade1fc52a1a471ff3e9acd2298899747edc2b8
revanthuradi <revanthuradi9866@gmail.com>
2024-07-24 17:09:29 +0530

    Initial commit

--- client/src/components/Statistics.jsx
+++ client/src/components/Statistics.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from 'react'
 import { PieChart } from '@mui/x-charts/PieChart';
 import { useAuth } from '../Context/AuthContext'
 import formatNumberIndian from '../Hooks/formatter';
+import toast from 'react-hot-toast';
+import Loader from './Loader';
 const Statistics = () => {
     const { userData } = useAuth()
     const [transactionType, setTransactionType] = useState('today')
     const [totalIncome, setTotalIncome] = useState(0)
+    const [isloading, setIsloading] = useState(false)
     const [data, setData] = useState([])
     const colors = [
         "#A06CE5",
@@ -19,6 +22,7 @@ const Statistics = () => {
     const getStats = async () => {
         try {
             const dat = []
+            setIsloading(true)
             const res = await axios.get(`https://penny-tracker-server.vercel.app/api/gettransactions/statistics/${transactionType}?id=${userData._id}`, {
                 headers: {
                     'Authorization': `Bearer ${localStorage.getItem('token')}`
@@ -34,8 +38,10 @@ const Statistics = () => {
             })
             setTotalIncome(res.data.totalIncome)
             setData(dat)
-
+            setIsloading(false)
         } catch (err) {
+            setIsloading(false)
+            toast.error('unable to fetch data')
             console.log(err)
         }
     }
@@ -67,47 +73,48 @@ const Statistics = () => {
             </div>
             <div className=''>
                 {
-                    data.length !== 0 ? <div>
-                        <div className='flex justify-center  py-6'>
-                            <PieChart
-                                series={[
+                    isloading ? <Loader /> :
+                        data.length !== 0 ? <div>
+                            <div className='flex justify-center  py-6'>
+                                <PieChart
+                                    series={[
 
-                                    {
-                                        innerRadius: 30,
-                                        outerRadius: 100,
-                                        paddingAngle: 5,
-                                        cornerRadius: 5,
-                                        startAngle: 180,
-                                        endAngle: -90,
-                                        cx: 190,
-                                        cy: 100,
-                                        data,
-                                        highlightScope: { faded: 'global', highlighted: 'item' },
-                                        faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-                                    },
-                                ]}
-                                slotProps={{
-                                    legend: {
-                                        hidden: true
-                                    },
-                                }}
-                                width={400}
-                                height={250}
-                            />
-                        </div>
-                        <div className='text-center py-3 text-lg font-medium flex  justify-center gap-2'>
-                            <p className='text-gray-500'>Total Money In :</p>
-                            <p>${totalIncome}</p>
-                        </div>
-                        <div className='flex gap-4 flex-wrap px-6 py-3  justify-center  '>
-                            {
-                                data.map(ele => <div style={{ backgroundColor: ele.color }} className={`px-2 py-2 text-darkPurple rounded-lg min-w-24 flex flex-col items-center`}>
-                                    <p className='capitalize'>{ele.label}</p>
-                                    <p>{formatNumberIndian(ele.value)}</p>
-                                </div>)
-                            }
-                        </div>
-                    </div> : <div className='text-lg font-medium text-center mt-8'>No Data Available</div>
+                                        {
+                                            innerRadius: 30,
+                                            outerRadius: 100,
+                                            paddingAngle: 5,
+                                            cornerRadius: 5,
+                                            startAngle: 180,
+                                            endAngle: -90,
+                                            cx: 190,
+                                            cy: 100,
+                                            data,
+                                            highlightScope: { faded: 'global', highlighted: 'item' },
+                                            faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                                        },
+                                    ]}
+                                    slotProps={{
+                                        legend: {
+                                            hidden: true
+                                        },
+                                    }}
+                                    width={400}
+                                    height={250}
+                                />
+                            </div>
+                            <div className='text-center py-3 text-lg font-medium flex  justify-center gap-2'>
+                                <p className='text-gray-500'>Total Money In :</p>
+                                <p>${totalIncome}</p>
+                            </div>
+                            <div className='flex gap-4 flex-wrap px-6 py-3  justify-center  '>
+                                {
+                                    data.map(ele => <div style={{ backgroundColor: ele.color }} className={`px-2 py-2 text-darkPurple rounded-lg min-w-24 flex flex-col items-center`}>
+                                        <p className='capitalize'>{ele.label}</p>
+                                        <p>{formatNumberIndian(ele.value)}</p>
+                                    </div>)
+                                }
+                            </div>
+                        </div> : <div className='text-lg font-medium text-center mt-8'>No Data Available</div>
                 }
             </div>
         </div>
